0b3ff9f624f922b35d2252e40780c4a38f71128c
Quy Thai <qthai@us.maxnerva.com>
2025-02-20 14:38:05 +0700

    Template method pattern

---
+++ java-language/src/design_patterns/behavioral/template_method/Demo.java
@@ -0,0 +1,39 @@
+package design_patterns.behavioral.template_method;
+
+import design_patterns.behavioral.template_method.networks.Facebook;
+import design_patterns.behavioral.template_method.networks.Network;
+import design_patterns.behavioral.template_method.networks.Twitter;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+/**
+ * Demo class. Everything comes together here.
+ */
+public class Demo {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new java.io.InputStreamReader(System.in));
+        Network network = null;
+        System.out.print("Input user name: ");
+        String userName = reader.readLine();
+        System.out.print("Input password: ");
+        String password = reader.readLine();
+
+        // Enter the message.
+        System.out.print("Input message: ");
+        String message = reader.readLine();
+
+        System.out.println("\nChoose social network for posting message.\n" +
+                "1 - Facebook\n" +
+                "2 - Twitter");
+        int choice = Integer.parseInt(reader.readLine());
+
+        // Create the proper network object and send the message.
+        if (choice == 1) {
+            network = new Facebook(userName, password);
+        } else if (choice == 2) {
+            network = new Twitter(userName, password);
+        }
+        network.post(message);
+    }
+}
